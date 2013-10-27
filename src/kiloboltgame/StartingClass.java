2a905e6880b307f117fd155f473ad693610df757
Tom Atnip <tom.atnip@gmail.com>
2013-10-27 16:25:25 -0400

    Finished Unit 2 Day 2

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -1,17 +1,24 @@
 package kiloboltgame;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
 
-public class StartingClass extends Applet {
-	
+public class StartingClass extends Applet implements Runnable {
+
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
@@ -22,5 +29,18 @@ public class StartingClass extends Applet {
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			repaint();
+
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
