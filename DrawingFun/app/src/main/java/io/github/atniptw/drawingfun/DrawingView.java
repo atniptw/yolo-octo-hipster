54f589d169e53001d3683137caca2ca8dae2d32a
Tom Atnip <tom.atnip@gmail.com>
2014-09-15 16:55:21 -0400

    Completed part two

--- DrawingFun/app/src/main/java/io/github/atniptw/drawingfun/DrawingView.java
+++ DrawingFun/app/src/main/java/io/github/atniptw/drawingfun/DrawingView.java
@@ -2,12 +2,15 @@ package io.github.atniptw.drawingfun;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 
@@ -24,6 +27,17 @@ public class DrawingView extends View {
     private float mTextWidth;
     private float mTextHeight;
 
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
     public DrawingView(Context context) {
         super(context);
         init(null, 0);
@@ -32,7 +46,7 @@ public class DrawingView extends View {
     public DrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setupDrawing();
-        init(attrs, 0);
+//        init(attrs, 0);
     }
 
     public DrawingView(Context context, AttributeSet attrs, int defStyle) {
@@ -40,9 +54,19 @@ public class DrawingView extends View {
         init(attrs, defStyle);
     }
 
-    private void setupDrawing(){
+    private void setupDrawing() {
         /* get drawing area setup for interaction */
+        drawPath = new Path();
+        drawPaint = new Paint();
 
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
 
     private void init(AttributeSet attrs, int defStyle) {
@@ -87,12 +111,20 @@ public class DrawingView extends View {
         mTextHeight = fontMetrics.bottom;
     }
 
+    //update color
+    public void setColor(String newColor){
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
+        /*
+        TODO: consider storing these as member variables to reduce
+        allocations per draw cycle.
         int paddingLeft = getPaddingLeft();
         int paddingTop = getPaddingTop();
         int paddingRight = getPaddingRight();
@@ -103,20 +135,59 @@ public class DrawingView extends View {
 
         // Draw the text.
         canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
+        paddingLeft + (contentWidth - mTextWidth) / 2,
+        paddingTop + (contentHeight + mTextHeight) / 2,
+        mTextPaint);
 
         // Draw the example drawable on top of the text.
         if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
+        mExampleDrawable.setBounds(paddingLeft, paddingTop,
+        paddingLeft + contentWidth, paddingTop + contentHeight);
+        mExampleDrawable.draw(canvas);
         }
+        */
+
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
     }
 
     /**
      * Gets the example string attribute value.
+     *
      * @return The example string attribute value.
      */
     public String getExampleString() {
@@ -126,6 +197,7 @@ public class DrawingView extends View {
     /**
      * Sets the view's example string attribute value. In the example view, this string
      * is the text to draw.
+     *
      * @param exampleString The example string attribute value to use.
      */
     public void setExampleString(String exampleString) {
@@ -135,6 +207,7 @@ public class DrawingView extends View {
 
     /**
      * Gets the example color attribute value.
+     *
      * @return The example color attribute value.
      */
     public int getExampleColor() {
@@ -144,6 +217,7 @@ public class DrawingView extends View {
     /**
      * Sets the view's example color attribute value. In the example view, this color
      * is the font color.
+     *
      * @param exampleColor The example color attribute value to use.
      */
     public void setExampleColor(int exampleColor) {
@@ -153,6 +227,7 @@ public class DrawingView extends View {
 
     /**
      * Gets the example dimension attribute value.
+     *
      * @return The example dimension attribute value.
      */
     public float getExampleDimension() {
@@ -162,6 +237,7 @@ public class DrawingView extends View {
     /**
      * Sets the view's example dimension attribute value. In the example view, this dimension
      * is the font size.
+     *
      * @param exampleDimension The example dimension attribute value to use.
      */
     public void setExampleDimension(float exampleDimension) {
@@ -171,6 +247,7 @@ public class DrawingView extends View {
 
     /**
      * Gets the example drawable attribute value.
+     *
      * @return The example drawable attribute value.
      */
     public Drawable getExampleDrawable() {
@@ -180,6 +257,7 @@ public class DrawingView extends View {
     /**
      * Sets the view's example drawable attribute value. In the example view, this drawable is
      * drawn above the text.
+     *
      * @param exampleDrawable The example drawable attribute value to use.
      */
     public void setExampleDrawable(Drawable exampleDrawable) {
--- DrawingFun/app/src/main/java/io/github/atniptw/drawingfun/MainActivity.java
+++ DrawingFun/app/src/main/java/io/github/atniptw/drawingfun/MainActivity.java
@@ -4,14 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
 
+    private DrawingView drawView;
+    private ImageButton currPaint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sample_drawing_view);
+
+        //get drawing view
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        //get the palette and first color button
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
 
 
@@ -32,5 +46,19 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+        //use chosen color
+        if(view!=currPaint){
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            //update ui
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
     }
 }
